aa32b3450bc47effe3767c878048a8deeac68475
César Ravelo Martínez <crmprograming@gmail.com>
2019-11-11 07:53:52 +0000

    Adaptados los ficheros de ETS a su contenido actual

--- TrimestrePrimero/ETS/PHP/funciones.php
+++ TrimestrePrimero/ETS/PHP/funciones.php
@@ -32,6 +32,47 @@
         }
         
         return ([$par, $impar]);
-    }    
+    }
+
+    // Devuelve un vector con todos los valores primos entre
+    // los rangos datos, ambos extremos incluidos
+    function obtenerPrimos($a, $b) {
+        // Vector con valores impares del rango dado
+        $impar = paresImpares($a, $b)[1];
+        define('TAM_IMPAR', sizeof($impar));
+        $primos = [];        
+        $i = 0;
+        $j = 2;
+        $pos1 = false;
+        
+        // Recorremos el listado de valores
+        // quedándonos sólo con los valores primos
+        for ($i = 0; $i < TAM_IMPAR; $i++) {
+            if ($impar[$i] == 1)
+                $primos[] = $impar[$i];
+            else {
+               $j = 2;
+                // Comprobamos si el valor actual es divisible
+                // por algún valor desde 2 hasta raíz cuadrada del valor
+                while ($j <= sqrt($impar[$i]) && ($impar[$i] % $j != 0))
+                    $j++;
+                if ($impar[$i] % $j != 0)
+                    $primos[] = $impar[$i];                    
+            }
+        }
+        
+        // En caso de que entre en el rango de $a y $b,
+        // insertamos el número 2 ya que nuestro vector
+        // en este punto contiene sólo números primos
+        $pos1 = array_search(1, $primos); 
+        if (is_int($pos1))
+            array_splice($primos, $pos1 + 1, 0, [2]);
+        
+        // Si el extremo inferior es 2, lo insertamos al inicio
+        // dado que en este punto nuestro vector sólo lo forman valores impares        
+        if ($a == 2)
+            array_splice($primos, 0, 0, [2]);
+        return ($primos);        
+    }
 
 ?>
--- TrimestrePrimero/ETS/PHP/numprimo.php
+++ TrimestrePrimero/ETS/PHP/numprimo.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="css/general.css">
 </head>
 <body>
-   <h1>Ejercicio 4: Mostrar números primos en un rango</h1>
+   <h1>Ejercicio 5: Mostrar números primos en un rango</h1>
    
    <form action="numprimo.php" method="get">
        <div class="formulario">
@@ -15,7 +15,7 @@
             </p>
             <ul>
                 <li>Valor a: <input type="text" name="a" value="1" placeholder="1"/></li>
-                <li>Valor b: <input type="text" name="b" value="1000" placeholder="1000"/></li>
+                <li>Valor b: <input type="text" name="b" value="100" placeholder="100"/></li>
                 <li><input type="submit" value="Mostrar Resultados" /></li>
             </ul>
        </div>
@@ -25,28 +25,19 @@
    
        <!-- CÓDIGO PHP -->
        <?php
+            include('funciones.php');
             // Definición de variables inicial
             $a = (isset($_GET["a"]))?$_GET["a"]:1;
-            $b = (isset($_GET["b"]))?$_GET["b"]:1000;            
-            $numeros = [];
-            $i = 0;
+            $b = (isset($_GET["b"]))?$_GET["b"]:100;            
+            $primos = [];            
        
-            // Generamos el vector con los datos almacenados       
-            for ($i = $a; $i <= $b; $i++) 
-                $numeros[] = $i;
+            // Generamos la lista de los valores primos del rango
+            $primos = obtenerPrimos($a, $b);
        
             // Mostrar valores primos entre A y B
             echo "<h2>Valores primos entre ". $a ." y ". $b ."</h2>\n";
-            foreach ($numeros as $i)
-                if ($i == 1 || $i == 2)
-                    echo "<div class='cajaNumero'>". $i ."</div>\n"; 
-                else {
-                    $j = 2;                    
-                    while ($j <= sqrt($i) && ($i % $j != 0))
-                        $j++;
-                    if ($i % $j != 0)
-                        echo "<div class='cajaNumero'>". $i ."</div>\n"; 
-                }            
+            foreach ($primos as $i)
+                echo "<div class='cajaNumero'>". $i ."</div>\n";                         
        
        ?>
     </div>
