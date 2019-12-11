6d365d413724f01ad1031b4092ad1e3dcc06ebec
César Ravelo Martínez <crmprograming@gmail.com>
2019-12-11 08:18:17 +0000

    Mejorado el programa para controlar más excepciones

--- TrimestrePrimero/PROGRAM/TrimestrePrimeroModular/src/Principal.java
+++ TrimestrePrimero/PROGRAM/TrimestrePrimeroModular/src/Principal.java
@@ -1,32 +1,33 @@
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import funciones.matematicas.Matematicas;
 
 public class Principal {
-	
+
 	/**
-	 * Dado un vector de datos y una cantidad, devuelve otro vector
-	 * con todos esos datos del mismo tamaño que la cantidad indicada
+	 * Dado un vector de datos y una cantidad, devuelve otro vector con todos esos
+	 * datos del mismo tamaño que la cantidad indicada
 	 * 
-	 * @param datos Vector con los valores a copiar
+	 * @param datos    Vector con los valores a copiar
 	 * @param cantidad Tamaño del vector a copiar
 	 * @return Vector del tamaño indicado con los valores ya copiados
 	 */
 	public static float[] ajustarVector(float[] datos, int cantidad) {
 		float[] v = new float[cantidad];
 		int i;
-		
+
 		for (i = 0; i < cantidad; i++)
 			v[i] = datos[i];
-		
+
 		return (v);
 	}
-	
+
 	/**
-	 * Método encargado de probar el código donde se utilizan bloques
-	 * try-catch a fin de controlar la excepción producida al calcular la media
-	 * de una secuencia vacía de valores.
+	 * Método encargado de probar el código donde se utilizan bloques try-catch a
+	 * fin de controlar la excepción producida al calcular la media de una secuencia
+	 * vacía de valores.
 	 */
 	public static void tryCatchMedia() {
 		final int LIMIT = 100, PARADA = 999;
@@ -34,48 +35,48 @@ public class Principal {
 		Scanner teclado = new Scanner(System.in);
 		int i = 0;
 		float actual = 0f;
-		
+
 		// Lectura de los n-elementos
-		System.out.printf("> Introduzca hasta %d elementos o %d:\n", LIMIT, PARADA);
-		System.out.printf("- Elemento %d: ", i + 1);
-		
-		try {		
-			actual = teclado.nextFloat();
-					
-			while (i < LIMIT - 1 && actual != PARADA) {
-				datos[i] = actual;
-				i++;
+		System.out.printf("> Introduzca hasta %d elementos o %d:\n", LIMIT, PARADA);		
+
+		do {
+			try {
 				System.out.printf("- Elemento %d: ", i + 1);
 				actual = teclado.nextFloat();
-			}
-			
-			if (actual != PARADA) {
 				datos[i] = actual;
 				i++;
+			} catch (InputMismatchException e) {
+				// En caso de haber leído un valor no numérico
+				teclado.next();
+			} catch (NoSuchElementException e) {
+				// En caso de terminarse el flujo de datos, forzamos a parar
+				actual = PARADA;
 			}
-			
-			teclado.close();
-			
-			// Ajustamos el vector reduciendo su tamaño hasta la cantidad de elementos insertada
-			datos = ajustarVector(datos, i);
-			
-			// Calculamos la media
-			try {
-				System.out.printf("> La media de los valores dados es %f\n", Matematicas.media(datos));
-			} catch(NullPointerException e) {
-				// En caso de intentar calcular la media de una secuencia vacía de valores
-				System.out.print("! Se produjo un error. ");
-				System.out.println("Esperada una lista de valores para calcular la media, no se obtuvo ninguno");
-			}
-		} catch(InputMismatchException e) {
-			// En caso de haber leído un valor no numérico
-			System.out.print("! Se produjo un error. ");
-			System.out.println("Esperado un valor numérico");
+		} while (i < LIMIT - 1 && actual != PARADA);
+
+		if (actual != PARADA) {
+			datos[i] = actual;
+			i++;
 		}
-		
+
+		teclado.close();
+
+		// Ajustamos el vector reduciendo su tamaño hasta la cantidad de elementos
+		// insertada
+		datos = ajustarVector(datos, i);
+
+		// Calculamos la media
+		try {
+			System.out.printf("> La media de los valores dados es %f\n", Matematicas.media(datos));
+		} catch (NullPointerException e) {
+			// En caso de intentar calcular la media de una secuencia vacía de valores
+			System.out.print("! Se produjo un error. ");
+			System.out.println("Esperada una lista de valores para calcular la media, no se obtuvo ninguno");
+		}
+
 	}
-	
-	public static void main(String[] args) {		
+
+	public static void main(String[] args) {
 		tryCatchMedia();
 	}
 
