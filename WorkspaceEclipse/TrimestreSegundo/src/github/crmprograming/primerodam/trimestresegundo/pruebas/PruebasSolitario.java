9adc968d9f214208be9078b9d63a5d6f133675fe
César Ravelo Martínnez <crmprograming@gmail.com>
2020-01-28 17:15:08 +0000

    Implementada la clase MontonPalo

--- WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/pruebas/PruebasSolitario.java
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/pruebas/PruebasSolitario.java
@@ -1,14 +1,23 @@
 package github.crmprograming.primerodam.trimestresegundo.pruebas;
 
-import github.crmprograming.primerodam.trimestresegundo.ejer.solitario.Mazo;
+import github.crmprograming.primerodam.trimestresegundo.ejer.solitario.*;
 
 public class PruebasSolitario {
 
 	public static void main(String[] args) {
 		Mazo m = new Mazo();
 		
+		/*
 		for (int i = 0; i < 40; i++)
 			System.out.println(m.sacarCarta());
+		*/
+		
+		MontonPalo mPalo = new MontonPalo();
+		
+		for (int i = 0; i < 40; i++)
+			mPalo.ponerCarta(m.sacarCarta());
+		
+		System.out.println();
 				
 
 	}
