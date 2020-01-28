992874ad17d94ea6e96068446b9ec574e54a5280
César Ravelo Martínez <crmprograming@gmail.com>
2020-01-28 14:12:10 +0000

    Create MontonPalo.java

---
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejer/solitario/MontonPalo.java
@@ -0,0 +1,18 @@
+package github.crmprograming.primerodam.trimestresegundo.ejer.solitario;
+
+public class MontonPalo {
+	
+	public static final int MAX_CARTAS = 10;
+	private Carta[] cartas;
+	private int cont;
+	
+	
+	public MontonPalo() {
+		cartas = new Carta[MAX_CARTAS];
+		cont = 0;
+	}
+	
+	
+	
+
+}
