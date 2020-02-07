32cfe199fadef7306b53cf9de53810c11aeb8c24
César Ravelo Martínez <crmprograming@gmail.com>
2020-02-07 12:27:30 +0000

    Creada la clase PilaEnterosCreciente

---
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/estructurasDatos/PilaEnterosCreciente.java
@@ -0,0 +1,37 @@
+package github.crmprograming.primerodam.trimestresegundo.estructurasDatos;
+
+/**
+ * Clase gestora de una PilaEnteros la cual guarda sus elementos
+ * de manera creciente.
+ * 
+ * @author César Ravelo Martínez
+ *
+ */
+public class PilaEnterosCreciente extends PilaEnteros {
+
+	/**
+	 * Constructor de la clase. Delega en el constructor
+	 * de la clase padre PilaEnteros
+	 * 
+	 * @param tam Entero con el tamaño máximo de la pila
+	 */
+	public PilaEnterosCreciente(int tam) {
+		super(tam);
+	}
+	
+	@Override
+	/**
+	 * Método encargado de insertar un valor al final de la pila.
+	 * Si la pila se encuentra llena o el valor es menor
+	 * al último almacenado, lanzará una IllegalStateException.
+	 * 
+	 * @param x Entero con el valor a almacenar
+	 */
+	public void apilar(int x) {
+		if (x < cima())
+			throw new IllegalStateException("ERROR. Valor dado superior al último en la pila");
+		
+		super.apilar(x);
+	}
+
+}
