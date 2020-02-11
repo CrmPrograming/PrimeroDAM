39d0b8b6b0dd79e0d962dfafe43c70a2f2b8eaa7
César Ravelo Martínez <crmprograming@gmail.com>
2020-02-11 09:49:38 +0000

    Realizado ejercicio de normalización de BAE y documentación para las clases del ejercicio Viajeros al tren

--- WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/Pasajero.java
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/Pasajero.java
@@ -2,16 +2,39 @@ package github.crmprograming.primerodam.trimestresegundo.ejerc.viajerosTren;
 
 import java.util.ArrayList;
 
+/**
+ * Clase gestora de una entidad Pasajero.
+ * No se podrá crear un pasajero cuyo nombre
+ * ya exista para otro.
+ * 
+ * @author César Ravelo Martínez
+ *
+ */
 public class Pasajero {
 	
+	/**
+	 * ArrayList<String> con el listado de pasajeros creados.
+	 */
 	private static ArrayList<String> pasajerosExistentes;
 	
+	/**
+	 * String con el nombre asociado al pasajero.
+	 */
 	private String nombre;
 	
+	/**
+	 * Inicialización estática del listado de pasajeros.
+	 */
 	static {
 		pasajerosExistentes = new ArrayList<String>();
 	}
 	
+	/**
+	 * Constructor de la clase. Inicializa el objeto
+	 * asignándole el nombre al pasajero.
+	 * 
+	 * @param nombre String con el nombre a asignarle al pasajero
+	 */
 	public Pasajero(String nombre) {
 		if (pasajerosExistentes.contains(nombre))
 			throw new IllegalStateException("ERROR: Ya existe un pasajero con el nombre " + nombre);
@@ -20,6 +43,11 @@ public class Pasajero {
 		pasajerosExistentes.add(nombre);
 	}
 	
+	/**
+	 * Método getter del atributo nombre.
+	 * 
+	 * @return String con el nombre del pasajero
+	 */
 	public String getNombre() {
 		return nombre;
 	}
--- WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/Tranvia.java
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/Tranvia.java
@@ -1,14 +1,38 @@
 package github.crmprograming.primerodam.trimestresegundo.ejerc.viajerosTren;
 
+/**
+ * Clase gestora del comportamiento de un tranvía.
+ * Un tranvía está compuesto por una serie de vagones
+ * cada uno con sus correspondientes pasajeros.
+ * 
+ * @author César Ravelo Martínez
+ *
+ */
 public class Tranvia {
-
-	private VagonPasajeros[] vagones;
+	
+	/**
+	 * Constante estática entera con la cantidad de vagones por defecto.
+	 */
 	private final static int CANTIDAD_VAGONES_DEFECTO = 4;
 	
+	/**
+	 * Tabla de VagonPasajeros con la cantidad de vagones a controlar.
+	 */
+	private VagonPasajeros[] vagones;
+	
+	/**
+	 * Constructor por defecto de la clase.
+	 */
 	public Tranvia() {
 		this(CANTIDAD_VAGONES_DEFECTO);	
 	}
 	
+	/**
+	 * Constructor inicializador de la clase.
+	 * Creará tantos vagones como se le especifique.
+	 * 
+	 * @param numVagones Entero con la cantidad de vagones a crear
+	 */
 	public Tranvia(int numVagones) {
 		int i;
 		
@@ -18,17 +42,39 @@ public class Tranvia {
 			vagones[i] = new VagonPasajeros();		
 	}
 	
+	/**
+	 * Método encargado de sacar un pasajero del vagón especificado.
+	 * El indicador del vagón irá en el rango [1, n].
+	 * 
+	 * @param nombre String con el nombre del pasajero a retirar
+	 * @param vagon Entero con el identificador del vagón a consultar
+	 * @return Instancia de Pasajero con el pasajero retirado
+	 */
 	public Pasajero bajar(String nombre, int vagon) {	
 		return vagones[vagon - 1].bajar(nombre);
 	}
 	
+	/**
+	 * Método encargado de subir un pasajero al vagón indicado.
+	 * El indicador del vagón irá en el rango [1, n].
+	 * - Si el vagón no existe, lanzará una RuntimeException
+	 * - Si no se pudo ubicar en ese vagón, se intentará colocar en los demás
+	 * - Si no se pudo ubicar en ningún vagón, lanzará una RuntimeException
+	 * - Si se colocó al pasajero, se retornará la posición donde se insertó
+	 * 
+	 * @param p Instancia de Pasajero con el pasajero a guardar
+	 * @param vagon Entero con el indicador del vagón a intentar insertar inicialmente
+	 * @return Entero con la posición del vagón donde se guardó el pasajero
+	 */
 	public int subir(Pasajero p, int vagon) {
 		int i = vagon - 1;
 		boolean ubicado = false;
 		
+		// Comprobamos si el vagón existe
 		if (i < 0 || i >= vagones.length)
 			throw new RuntimeException("ERROR: El vagón solicitado no existe.");
 		
+		// Mientras queden vagones, buscamos un asiento para el pasajero
 		do {
 			try {
 				vagones[i % vagones.length].subir(p);
@@ -36,8 +82,8 @@ public class Tranvia {
 			} catch(Exception e) {
 				i++;
 			}			
-		} while (!ubicado && i % vagones.length != vagon - 1);
-		
+		} while (!ubicado && i % vagones.length != vagon - 1);	
+
 		if (!ubicado)
 			throw new RuntimeException("ERROR: No quedan vagones libres para ubicar al pasajero.");
 		
