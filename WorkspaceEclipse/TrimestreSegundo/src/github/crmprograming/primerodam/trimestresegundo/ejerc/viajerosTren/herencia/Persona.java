cd468439ca5d4c85ce3bad904e4b654b5dd08dcc
César Ravelo Martínez <crmprograming@gmail.com>
2020-02-11 12:23:56 +0000

    Añadida la documentación a las clases del ejercicio Viajeros al tren con herencia

--- WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/herencia/Persona.java
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/herencia/Persona.java
@@ -2,16 +2,38 @@ package github.crmprograming.primerodam.trimestresegundo.ejerc.viajerosTren.here
 
 import java.util.ArrayList;
 
+/**
+ * Clase gestora de una entidad Persona.
+ * No se podrá crear una persona cuyo nombre
+ * ya exista para otro.
+ * 
+ * @author César Ravelo Martínez
+ */
 public class Persona {
 	
+	/**
+	 * ArrayList<String> con el listado de personas creados.
+	 */
 	private static ArrayList<String> pasajerosExistentes;
 	
+	/**
+	 * String con el nombre asociado a la persona.
+	 */
 	private String nombre;
 	
+	/**
+	 * Inicialización estática del listado de personas.
+	 */
 	static {
 		pasajerosExistentes = new ArrayList<String>();
 	}
 	
+	/**
+	 * Constructor de la clase. Inicializa el objeto
+	 * asignándole el nombre a la persona.
+	 * 
+	 * @param nombre String con el nombre a asignarle a la persona
+	 */
 	public Persona(String nombre) {
 		if (pasajerosExistentes.contains(nombre))
 			throw new IllegalStateException("ERROR: Ya existe un pasajero con el nombre " + nombre);
@@ -20,6 +42,11 @@ public class Persona {
 		pasajerosExistentes.add(nombre);
 	}
 	
+	/**
+	 * Método getter del atributo nombre.
+	 * 
+	 * @return String con el nombre de la persona
+	 */
 	public String getNombre() {
 		return nombre;
 	}
--- WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/herencia/Vagon.java
+++ WorkspaceEclipse/TrimestreSegundo/src/github/crmprograming/primerodam/trimestresegundo/ejerc/viajerosTren/herencia/Vagon.java
@@ -1,31 +1,71 @@
 package github.crmprograming.primerodam.trimestresegundo.ejerc.viajerosTren.herencia;
 
+/**
+ * Clase gestora de un vagón genérico, junto
+ * con sus métodos asociados al movimiento de pasajeros. 
+ * 
+ * @author César Ravelo Martínez
+ */
 public class Vagon {
 
+	/**
+	 * Constante estática entera para indicar una capacidad por defecto en los vagones.
+	 */
 	private final static int CAPACIDAD_POR_DEFECTO = 30;
 	
+	/**
+	 * Tabla de Persona con las personas ubicadas en el vagón.
+	 */
 	private Persona[] asientos;
 	
+	/**
+	 * Constructor por defecto de la clase. Inicializa la capacidad
+	 * del vagón a CAPACIDAD_POR_DEFECTO.
+	 */
 	public Vagon() {
 		this(CAPACIDAD_POR_DEFECTO);
 	}
 	
+	/**
+	 * Constructor de la clase inicializador de la capacidad del vagón
+	 * al parámetro indicado. Debe ser un valor positivo.
+	 * 
+	 * @param capacidad Entero con la capacidad a asignarle al vagón
+	 */
 	public Vagon(int capacidad) {
 		asientos = new Persona[capacidad];
 	}
 	
-	public void subir(Persona pasajero) {
+	/**
+	 * Método encargado de almacenar una persona en el vagón.
+	 * - Sólo insertará una persona si hay espacio.
+	 * - En caso de no haber espacio, lanzará RuntimeException
+	 * - Las personas se insertan de manera consecutiva en el primer
+	 * hueco que encuentre
+	 * 
+	 * @param persona Instancia de Persona a almacenar
+	 */
+	public void subir(Persona persona) {
 		int i = 0;
 		
 		while (i < asientos.length && asientos[i] != null)
 			i++;
 		
 		if (i < asientos.length)
-			asientos[i] = pasajero;
+			asientos[i] = persona;
 		else
 			throw new RuntimeException("ERROR: No queda espacio libre en el vagón.");
 	}
 	
+	/**
+	 * Método encargado de retirar y obtener una persona del vagón.
+	 * Se realiza la búsqueda de la persona por su nombre:
+	 * - Si se encontró, se extrae la persona de la tabla asientos
+	 * - En caso de no encontrarlo, retornará null.
+	 * 
+	 * @param nombre String con el nombre de la persona a buscar
+	 * @return Instancia de Persona con la persona extraída
+	 */
 	public Persona bajar(String nombre) {
 		Persona pVagon = null;
 		int i = 0;
