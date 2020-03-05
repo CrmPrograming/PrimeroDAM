59bdd56d9e00e411bd8e9fd996d1a9edc020de4b
César Ravelo Martínnez <crmprograming@gmail.com>
2020-03-05 16:52:59 +0000

    Update ScriptVistas3_Agenda1.sql

--- TrimestreSegundo/BAE/Vistas/ScriptVistas3_Agenda1.sql
+++ TrimestreSegundo/BAE/Vistas/ScriptVistas3_Agenda1.sql
@@ -16,14 +16,59 @@ SELECT Nombre, Fechacumple FROM v_provincias19 ORDER BY Fechacumple
 GO
 
 -- b) Cual es el contacto o contactos que están más próximos a cumplir años
-SELECT Nombre, (MONTH(Fechacumple) * 30 + DAY(Fechacumple)) - (MONTH(GETDATE()) * 30 + DAY(GETDATE())) AS [Días Pasados], Fechacumple FROM v_provincias19 
-WHERE (MONTH(Fechacumple) * 30 + DAY(Fechacumple)) - (MONTH(GETDATE()) * 30 + DAY(GETDATE())) >= 0
-ORDER BY [Días Pasados]
+SELECT TOP 1 WITH TIES Nombre, (MONTH(Fechacumple) * DAY(EOMONTH(Fechacumple)) + DAY(Fechacumple)) - (MONTH(GETDATE()) * DAY(EOMONTH(GETDATE())) + DAY(GETDATE())) AS [Días Restantes] FROM v_provincias19 
+WHERE (MONTH(Fechacumple) * DAY(EOMONTH(Fechacumple)) + DAY(Fechacumple)) - (MONTH(GETDATE()) * DAY(EOMONTH(GETDATE())) + DAY(GETDATE())) >= 0
+ORDER BY [Días Restantes]
 GO
 
-SELECT * FROM contactos
+-- c) Cual o cuales son la provincia con más contactos de la categoría X
+SELECT TOP 1 WITH TIES NombreProvincia, COUNT(Nombre) AS Total FROM v_provincias19
+WHERE Descripcion = 'Social'
+GROUP BY NombreProvincia
+ORDER BY Total DESC
+GO
 
-(MONTH(Fechacumple) * 30 + DAY(Fechacumple)) - (MONTH(GETDATE()) * 30 + DAY(GETDATE()))
+-- d) Cual o cuales son la provincia con contactos de la categoría X y más de 1 contacto
+SELECT TOP 1 WITH TIES NombreProvincia, COUNT(Nombre) AS Total FROM v_provincias19
+WHERE Descripcion = 'Social'
+GROUP BY NombreProvincia
+HAVING COUNT(Nombre) > 1
+ORDER BY Total DESC
+GO
 
-SELECT Fechacumple, DAY(EOMONTH(Fechacumple)) FROM v_provincias19
+-- e) Visualiza todos los contactos de más de 18 años
+SELECT * FROM v_provincias19
+WHERE YEAR(GETDATE()) - YEAR(Fechacumple) >= 19 OR 
+	(YEAR(GETDATE()) - YEAR(Fechacumple) = 18 AND (MONTH(Fechacumple) * DAY(EOMONTH(Fechacumple)) + DAY(Fechacumple)) - (MONTH(GETDATE()) * DAY(EOMONTH(GETDATE())) + DAY(GETDATE())) <= 0)
+GO
+-- El criterio del WHERE busca aquellos que tengan cumplidos 19 o más años o los que cumplen este año 18 y ya fue su cumple
 
+-- f) Visualiza todas las categorías con más de 1 contacto
+SELECT Descripcion, COUNT(Nombre) AS [Total contactos] FROM v_provincias19
+GROUP BY Descripcion
+HAVING COUNT(Nombre) > 1
+GO
+
+-- g) Visualiza todas las categorías de más de 1 contacto que todos sean mayores de edad.
+SELECT Descripcion FROM v_provincias19
+WHERE YEAR(GETDATE()) - YEAR(Fechacumple) >= 19 OR 
+	(YEAR(GETDATE()) - YEAR(Fechacumple) = 18 AND (MONTH(Fechacumple) * DAY(EOMONTH(Fechacumple)) + DAY(Fechacumple)) - (MONTH(GETDATE()) * DAY(EOMONTH(GETDATE())) + DAY(GETDATE())) <= 0)
+GROUP BY Descripcion
+HAVING COUNT(Nombre) > 1
+GO
+
+-- h) Cual es la población con menos contactos
+SELECT TOP 1 Poblacion FROM v_provincias19
+WHERE Poblacion IS NOT NULL
+GROUP BY Poblacion
+ORDER BY COUNT(Nombre)
+GO
+
+-- 21. Crea una vista que Visualice todas las categorías de más de 1 contacto que todos sean mayores de edad.
+CREATE VIEW dbo.v_vistasagenda21 AS
+	SELECT Descripcion FROM v_provincias19
+	WHERE YEAR(GETDATE()) - YEAR(Fechacumple) >= 19 OR 
+		(YEAR(GETDATE()) - YEAR(Fechacumple) = 18 AND (MONTH(Fechacumple) * DAY(EOMONTH(Fechacumple)) + DAY(Fechacumple)) - (MONTH(GETDATE()) * DAY(EOMONTH(GETDATE())) + DAY(GETDATE())) <= 0)
+	GROUP BY Descripcion
+	HAVING COUNT(Nombre) > 1
+GO
